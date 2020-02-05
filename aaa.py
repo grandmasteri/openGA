3a003c42ddad0e90c1a0b466636daf0593dd00e1
Arash-codedev <arash.codedev@gmail.com>
2020-02-05 23:46:00 +1100

    update

---
+++ aaa.py
@@ -0,0 +1,6 @@
+import os
+cfiles = []
+for root, dirs, files in os.walk('C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\'):
+  for file in files:
+    if file.endswith('.exe'):
+      print(os.path.join(root, file))
