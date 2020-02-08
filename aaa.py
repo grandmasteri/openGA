08e2ba0def50b621f29fd779d4c92bd95a813dcb
Arash-codedev <arash.codedev@gmail.com>
2020-02-08 19:51:43 +1100

    try

--- aaa.py
+++ aaa.py
@@ -1,6 +1,6 @@
 import os
 cfiles = []
 for root, dirs, files in os.walk('C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\'):
-  for file in dirs:
-    if file.find('nclude')>=0:
+  for file in files:
+    if file.startswith('iostream'):
       print(os.path.join(root, file))
