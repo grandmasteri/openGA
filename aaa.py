06f511bb75ff1dee232db1d3687ae8d57558c9f6
Arash-codedev <arash.codedev@gmail.com>
2020-02-20 23:08:30 +1100

    try

--- aaa.py
+++ aaa.py
@@ -3,5 +3,5 @@ cfiles = []
 # for root, dirs, files in os.walk('C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\'):
 for root, dirs, files in os.walk('C:\\Program Files (x86)\\Windows Kits'):
   for file in files:
-    if file.startswith('uuid.lib'):
+    if file.endswith('.lib'):
       print(os.path.join(root, file))
