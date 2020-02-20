b271af0c1a00795f151a196a878db2215d565c1c
Arash-codedev <arash.codedev@gmail.com>
2020-02-20 18:08:42 +1100

    try

--- aaa.py
+++ aaa.py
@@ -2,5 +2,5 @@ import os
 cfiles = []
 for root, dirs, files in os.walk('C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\'):
   for file in files:
-    if file.startswith('iostream'):
+    if file.startswith('clues.dll'):
       print(os.path.join(root, file))
